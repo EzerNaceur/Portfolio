7c9697ea6415a9ace5b972647ab109fe95c2b671
N00ber17 <azernaceur17@gmail.com>
2024-05-10 17:12:03 +0100

    Adjusted position of canvas + Added the mobile device listener for scale and position at 500px width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,24 @@ const Hero = () => {
 
         <ComputersCanvas />
 
-        <div></div>
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+          <a href='#about'>
+            <div className='w-[35px] h-[64px] border-4 rounded-3xl border-white flex justify-center items-start p-2'>
+              <motion.dev
+                animate={{
+                  y: [0, 24, 0]
+                }} 
+                transition={{
+                  duration: 1.5,
+                  repeat: Infinity,
+                  repeatType: 'loop'
+                }}
+                className="w-3 h-3 rounded-full bg-white mb-1"
+                />
+
+            </div>
+          </a>
+        </div>
     </section>
   )
 }
