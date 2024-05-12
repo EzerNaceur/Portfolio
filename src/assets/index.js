9447786745dd349dcb95a2906c524a83398b8750
N00ber17 <azernaceur17@gmail.com>
2024-05-12 19:40:42 +0100

    Replaced the default icons as well as started working on the balls section - no homo

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import fullstack from "./company/fullstack.png";
+import team from "./company/team.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +50,8 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  team,
+  fullstack,
   carrent,
   jobit,
   tripguide,
